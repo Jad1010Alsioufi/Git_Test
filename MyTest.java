4c49f17bfaea22405a4b097281ae010966980b33
Jad <jad.alsioufi@gmail.com>
2025-05-25 15:48:29 +0200

    Changing in MyTest java file

--- MyTest.java
+++ MyTest.java
@@ -1,7 +1,7 @@
 public class MyTest {
     public static void main(String[] args) {
 
-        String part = "abc-def";
+        String part = "abc-def-545";
         String[] array = part.split("\\-");
         for (String string : array) {
             System.out.println(string);
